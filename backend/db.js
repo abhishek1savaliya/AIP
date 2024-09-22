fae7c6fc85cf9e545d883e89ea01e05ae0b2f041
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2024-09-22 11:40:34 +0530

    Update dialect

--- backend/db.js
+++ backend/db.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
-        dialect: process.env.DB_DIALECT,
+        dialect: 'postgres',
         dialectOptions: {
             ssl: {
                 require: process.env.SSL_REQUIRE === 'true',
